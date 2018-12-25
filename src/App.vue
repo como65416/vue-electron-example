0035be90409e9ca5dae5ea5aaceff3701c1c3b24
comoco <como65416@gmail.com>
2018-12-25 23:25:05 +0800

    add src example

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <component-a></component-a>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import ComponentA from './components/ComponentA.vue';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  components: {
+    'component-a': ComponentA
+  }
+}
+</script>
---
+++ src/components/ComponentA.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Component A
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  components: {
+  },
+  mounted: function () {
+  }
+}
+</script>
